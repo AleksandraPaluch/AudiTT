71024c5e1cc9f77ad6f7c84991f7296f98bb92ee
RamzesGrucha <wiktorr53@gmail.com>
2022-03-24 23:06:43 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="ola",
+    version='0.1',
+    description='biblioteka',
+    url='',
+    author='ola paluch',
+    author_email='olapaluch2@gmail.com',
+    license='unlicense',
+    packages=['projekty'],
+    zip_safe=False
+)
